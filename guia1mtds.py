6199e6a77026ee950466070e72e55cc5d327fd8d
Pili Solari <psolaribarrios@mail.utdt.edu>
2023-04-13 15:19:02 -0300

    iniciotp1

--- guia1mtds.py
+++ guia1mtds.py
@@ -1,3 +1,4 @@
+
 #EJERCICIO 1
 ##suma de vectores en R3##
 def sumavectores(v1, v2):
@@ -108,17 +109,48 @@ import numpy as np
 #ax.plot(x, np.ones_like(x)*2)
 
 #7
-def graficador(b, v):
-    x = np.linspace(0,10,20)
-    fig, ax = plt.subplots(figsize=(5, 2.7), layout="constrained")
+# def graficador(b, v):
+#     x = np.linspace(0,10,20)
+#     fig, ax = plt.subplots(figsize=(5, 2.7), layout="constrained")
 
-    ax.plot(x, (3/2)*x - b/2)
-    ax.plot(x, (3/2)*x - v/4)
+#     ax.plot(x, (3/2)*x - b/2)
+#     ax.plot(x, (3/2)*x - v/4)
 
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('ejercicio 6')
-    plt.show()
+#     plt.xlabel('x')
+#     plt.ylabel('y')
+#     plt.title('ejercicio 6')
+#     plt.show()
 
-print(graficador(1, 1))
+# print(graficador(1, 1))
+
+#A.dot(vector)
+
+def escalar(vec, a, b):
+    A = np.array([[a,0],[0,b]])
+    return A.dot(vec)
+
+def rotar(vec):
+    A = np.array([[0,1],[1,0]])
+    return A.dot(vec)
+
+def proyectar(vec):
+    A = np.array([[0,0],[0,1]])
+    return A.dot(vec)
+
+def reflejar(vec):
+    A = np.array([[-1,0],[0,-1]])
+    return A.dot(vec)
+
+res = escalar([1,1],5,5)
+
+plt.title("Matplotlib demo") 
+plt.xlabel("x") 
+plt.ylabel("y") 
+# plt.axhline(0, color="black")
+# plt.axvline(0, color="black")
+plt.quiver(0,0,res[0],res[1], angles='xy', scale_units='xy', scale=1)
+plt.grid(True)
+plt.xlim([-10,10])
+plt.ylim([-10,10])
+plt.show()
 
